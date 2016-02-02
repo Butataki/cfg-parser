c7bbcf94fc976c30102c25cf8824ccc5813f2ee1
Roman Shuvalov <rshuvalov@abtronics.ru>
2016-02-02 14:30:09 +0300

    Add setup.py for package building

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="CfgParser",
+    version="1.0.0",
+    packages=['cfg_parser'],
+    # metadata
+    author="Roman Shuvalov",
+    author_email="orangato@yandex.ru",
+    description="Parsing config files with predetermined structure and generating default one if no config file found.",
+    keywords="configuration cfg config",
+)
